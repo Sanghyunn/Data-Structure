dabe6acac0ccd77f7a1dfd0a865b7cf97f3fe912
Sanghyunn <shawnpark98@naver.com>
2020-11-08 22:43:38 +0900

    Search Function Added

--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -16,20 +16,22 @@ class List{
  private :
 	Node * Head;
 	Node * Tail;
+	int index;
 
  public :
-	List(){Head = NULL; Tail = NULL;}
+	List(){Head = NULL; Tail = NULL; index = 0;}
 	int Choice();
 	void Add(int);
-	void Print();};
+	void Print();
+	void Search(int);};
 
 int List::Choice(){
 	int n = 0;
 
 	
-	while( n != 1 && n != 2 && n != 3 ){
-		cout << "Choose (1) Add / (2) Print / (3) End : "; cin >> n; 
-		if( n != 1 && n != 2 && n != 3 ) cout << "Plz Choose Number CORRECTLY\n" << endl;}
+	while( n != 1 && n != 2 && n != 3 && n != 4){
+		cout << "Choose (1) Add / (2) Print / (3) End / (4) Search : "; cin >> n; 
+		if( n != 1 && n != 2 && n != 3 && n != 4) cout << "Plz Choose Number CORRECTLY\n" << endl;}
 	return n;} 
 
 void List::Add(int a){
@@ -46,6 +48,8 @@ void List::Add(int a){
 		Tail -> next = Temp;
 		Tail = Temp;}
 
+	index++;
+
 	cout << "Number "<< a << " is Added\n" << endl;}
 
 void List::Print(){
@@ -53,14 +57,36 @@ void List::Print(){
 		cout << "List is Emtpy\n" << endl;}
 
 	else{
+		int cnt = 1;
 		Node * ps = Head;
-		cout << "[HEAD] " << ps -> data;
+		cout << "[HEAD] (" << cnt << ") " << ps -> data;
 		ps = ps -> next;
 
 		while(ps != NULL){
-			cout << " -> " << ps -> data;
+			cnt++;
+			cout << " -> (" << cnt << ") " << ps -> data;
 			ps = ps -> next;}
 		cout << " [TAIL]\n" << endl;}}
+
+
+void List::Search(int i){
+	if(Head == NULL){
+		cout << "Unavailable to Search (List is Empty)\n" << endl;}
+
+	else if(i > index){
+		cout << "Unavailable to Search (Index Overflow)\n" << endl;}
+
+	else{
+		Node *ps = Head;
+		
+		for(int j = 1; j < i; j++){
+			ps = ps -> next;}
+
+		cout << i << " index's data : " << ps -> data << '\n' << endl;}}	
+
+	
+			
+			
 
 int main(){
 	List list;
@@ -77,8 +103,12 @@ int main(){
 		else if(ps == 2){
 			list.Print();}
 
-		else if(ps == 3) break;}
+		else if(ps == 3) break;
 
+		else if(ps == 4){
+			int index;
+			cout << "Index to Search : "; cin >> index;
+			list.Search(index);}}
 
 	cout << "Linked List End" << endl;
 
